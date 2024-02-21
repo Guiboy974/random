a5740eed6225b277b8f819c511cbde1f28b63f4c
Guillaume Boyer <guiboy97.410@gmail.com>
2024-02-21 14:42:58 +0100

    swicht1

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ function RandCountry() {
     let country = ["Chinois","Francais","Italien","Japonais","Mexicain","Thailandais"];
     let random = country[Math.floor(Math.random()*country.length)];
     document.getElementById('repas').innerHTML = "on mange " + random;
+
+    switch (random) {
+        case "Chinois":
+        console.log("riz cantonais, sauté de nouille, ...");
+        break;
+        case "Francais":
+        console.log("risotto, raclette, boeuf bourguignon, ...");
+        break;
+        default:
+        console.log(`Désolé, nous n'avons rien à vous proposé pour le menu ${random}.`);
+    }
 }
 bountonRandom.addEventListener ("click", RandCountry)
+
+    
+    
